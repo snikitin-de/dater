5ffbc1d654deb029a42031d540a818413317d8c4
Sergey Nikitin <snikitin.de@gmail.com>
2020-12-31 12:26:16 +0300

    Add a class to describe the metadata of a dataset

---
+++ Model/DatasetMetaInfo.cs
@@ -0,0 +1,35 @@
+﻿namespace dater
+{
+    class DatasetMetaInfo
+    {
+        int rowCount;
+        string outputFile;
+        string separator;
+
+
+        public int RowCount
+        {
+            get { return rowCount; }
+            set { rowCount = value; }
+        }
+
+        public string Separator
+        {
+            get { return separator; }
+            set { separator = value; }
+        }
+
+        public string OutputFile
+        {
+            get { return outputFile; }
+            set { outputFile = value; }
+        }
+
+        public DatasetMetaInfo(int rowCount, string separator, string outputFile)
+        {
+            RowCount = rowCount;
+            Separator = separator;
+            OutputFile = outputFile;
+        }
+    }
+}
